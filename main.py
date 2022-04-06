c763924b604ebd79441c52b60540cc8c12b3fe68
tom <t-gutowski@o2.pl>
2022-04-06 21:07:33 +0200

    refactor(main):remove Factory from main

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
 import os
 
-from kivy.factory import Factory
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager
 from kivymd.app import MDApp
-
-from src.multiplication import Multiply
 
 
 class Main_App(MDApp):
@@ -19,9 +16,6 @@ class Main_App(MDApp):
         kv_files = os.listdir("kv")
         for f in kv_files:
             self.screen_manager.add_widget(Builder.load_file("kv/" + f))
-
-        # Register custom classes.
-        Factory.register(Multiply, cls="Multiply")
 
         return self.screen_manager
 

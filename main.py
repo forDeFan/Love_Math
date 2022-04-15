9eeedc5aaef210cf5a642cafaaa98c4412377e77
tom <t-gutowski@o2.pl>
2022-04-15 17:49:27 +0200

    feat(main.py): add factory aproach, remodel kv loading

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-import os
-
 from kivy.factory import Factory
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager
 from kivymd.app import MDApp
 
+from src.helpers import Helpers
 from src.multiplication import Multiply
 
 
@@ -13,14 +12,21 @@ class Main_App(MDApp):
         # Setup app defaults details.
         self.title = "I LoVE MatH"
         self.theme_cls.primary_palette = "Pink"
-
         self.screen_manager = ScreenManager()
-        # Load all files from kv folder and add to manager.
-        kv_files = os.listdir("kv")
-        for f in kv_files:
-            self.screen_manager.add_widget(Builder.load_file("kv/" + f))
-        # Register custom classes.
-        Factory.register(Multiply, cls="Multiply")
+
+        # Load main screen at app startup.
+        self.screen_manager.add_widget(
+            Builder.load_file("kv/main_screen.kv")
+        )
+        self.screen_manager.current = "main_screen"
+        # Load rest of kv's.
+        self.screen_manager.add_widget(
+            Builder.load_file("kv/multiplication_screen.kv")
+        )
+
+        # Register custom classes to use in kv's.
+        Factory.register(Multiply, "Multiply")
+        Factory.register(Helpers, "Helpers")
 
         return self.screen_manager
 

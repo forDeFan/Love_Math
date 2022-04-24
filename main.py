278db2e0dda70212ad14c82ceec15f82c40eca0c
tom <t-gutowski@o2.pl>
2022-04-24 16:12:46 +0200

    feat(db): add db global instance in Main_App

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
+import os
+
 from kivy.factory import Factory
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager
+from kivy.utils import platform
 from kivymd.app import MDApp
 
+from src.db_connection import Db_Connection
 from src.helpers import Helpers
 from src.multiplication import Multiply
 
@@ -12,23 +16,48 @@ class Main_App(MDApp):
         # Setup app defaults details.
         self.title = "I LoVE MatH"
         self.theme_cls.primary_palette = "Pink"
+
         self.screen_manager = ScreenManager()
 
         # Load main screen at app startup.
         self.screen_manager.add_widget(
             Builder.load_file("kv/main_screen.kv")
         )
-        self.screen_manager.current = "main_screen"
+
         # Load rest of kv's.
         self.screen_manager.add_widget(
             Builder.load_file("kv/multiplication_screen.kv")
         )
 
-        # Register custom classes to use in kv's.
+        # Register custom classes tp use them in kv's.
         Factory.register(Multiply, "Multiply")
         Factory.register(Helpers, "Helpers")
 
+        # Start global DB.
+        self.db = Db_Connection(
+            "my.db",
+            "multiply",
+            "fraction",
+            "percent",
+            "roman",
+        )
+
         return self.screen_manager
+
+    def on_start(self):
+        self.screen_manager.current = "main_screen"
+
+    def on_stop(self):
+        db_file = "my.db"
+        if os.path.isfile(db_file):
+            os.remove(db_file)
+
+    def update_db_result(self, category_name: str, is_true: bool):
+        if is_true:
+            self.db.update_result(category_name)
+
+    def get_db_result(self, category_name: str) -> str:
+        return str(self.db.get_result(category_name)[0])
 
 
 if __name__ == "__main__":

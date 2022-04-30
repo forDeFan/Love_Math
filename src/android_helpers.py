a5499973afeaa4ac80278e2b7576d95ded9f4704
tom <t-gutowski@o2.pl>
2022-04-30 18:56:31 +0200

    feat(android helpers): implement basic methods

---
+++ src/android_helpers.py
@@ -0,0 +1,17 @@
+from plyer import sms
+from kvdroid.tools import toast
+from kvdroid.tools.contact import get_contact_details
+
+
+class Android_Helpers:
+    def get_ph_book(self):
+        return get_contact_details("phone_book")
+
+    def get_phone_no(self, name):
+        return get_contact_details(name)
+
+    def show_toast(self, text):
+        toast(text)
+
+    def send_sms(self, tel, msg):
+        sms.send(recipient=str(tel), message=msg)

48e0ca4c991c3ee37b857e49347f195dd893b56a
tom <t-gutowski@o2.pl>
2022-05-01 10:55:49 +0200

    feat(android): add android import and send sms method

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ from kivy.utils import platform
 from kivymd.app import MDApp
 
 from src.db_connection import Db_Connection
-from src.ui_helpers import Ui_Helpers
 from src.multiplication import Multiply
+from src.ui_helpers import Ui_Helpers
 
 
 class Main_App(MDApp):
@@ -16,8 +16,8 @@ class Main_App(MDApp):
         # Get android permissions if run on android.
         if platform == "android":
             from android.permissions import (
-                request_permissions,
                 Permission,
+                request_permissions,
             )
 
             request_permissions(
@@ -28,6 +28,11 @@ class Main_App(MDApp):
                     Permission.SEND_SMS,
                 ]
             )
+            from src.android_helpers import Android_Helpers
+
+            # Set android communication interface.
+            self.phone = Android_Helpers()
+
         # Setup app defaults details.
         self.title = "I LoVE MatH"
         self.theme_cls.primary_palette = "Pink"
@@ -73,6 +78,10 @@ class Main_App(MDApp):
 
     def get_db_result(self, category_name: str) -> str:
         return str(self.db.get_result(category_name)[0])
+
+    def send_result_sms(self, tel, msg):
+        if platform == "android":
+            self.phone.send_sms(tel=tel, msg=msg)
 
 
 if __name__ == "__main__":

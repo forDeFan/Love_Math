e641fcefc9a6f274a5d746f478cc30a9084e73d1
tom <t-gutowski@o2.pl>
2022-06-02 17:48:48 +0200

    feat(lang): add categories in polish

--- src/constants.py
+++ src/constants.py
@@ -1,3 +1,5 @@
 APP_NAME = "I LoVE MatH"
 DB_NAME = "my.db"
+# Categories en and pl have to correspond eachother at specific index.
 CATEGORIES = ["multiply", "fraction", "percent", "roman"]
+CATEGORIES_PL = ["mnożenie", "ułamki", "procenty", "liczby rzymskie"]

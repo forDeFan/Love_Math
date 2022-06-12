f37854d62f1a41921ed8a53feff23cdbd50b4417
tom <t-gutowski@o2.pl>
2022-06-12 19:57:41 +0200

    feat(languages): add APP_LANG

--- src/constants.py
+++ src/constants.py
@@ -1,5 +1,6 @@
 APP_NAME = "I LoVE MatH"
 DB_NAME = "my.db"
+APP_LANG = ["pl", "en"]
 # Categories en and pl have to correspond eachother at specific index.
 CATEGORIES = ["multiply", "fraction", "percent", "roman"]
 CATEGORIES_PL = ["mnożenie", "ułamki", "procenty", "liczby rzymskie"]

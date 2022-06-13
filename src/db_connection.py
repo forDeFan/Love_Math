5434d1d6923f3588a4165ecb06a970d996d99b66
tom <t-gutowski@o2.pl>
2022-06-13 20:54:17 +0200

    feat(percentage): add methods for percentage calculation

--- src/db_connection.py
+++ src/db_connection.py
@@ -10,12 +10,13 @@ class Db_Connection:
             """CREATE TABLE IF NOT EXISTS results(
             category text,
             result integer,
+            questions_no integer,
             UNIQUE(category))"""
         )
         # Add categories from list.
         for c in categories:
             self.cur.execute(
-                f"INSERT OR IGNORE INTO results(category,result) VALUES ('{c}','0')"
+                f"INSERT OR IGNORE INTO results(category,result,questions_no) VALUES ('{c}','0','0')"
             )
 
         self.conn.commit()
@@ -38,3 +39,24 @@ class Db_Connection:
     def get_results(self) -> List[Tuple[str, str]]:
         res = self.cur.execute("SELECT * FROM results")
         return res.fetchall()
+
+    def update_question_no(self, category_name: str) -> None:
+        self.cur.execute(
+            f"UPDATE results SET questions_no=questions_no+1 WHERE category='{category_name}'"
+        )
+        self.conn.commit()
+
+    def get_question_no(self, category_name: str) -> str:
+        res = self.cur.execute(
+            f"SELECT questions_no FROM results WHERE category='{category_name}'"
+        )
+        return res.fetchone()
+
+    def get_percent(self, category_name):
+        q_no = self.get_question_no(category_name=category_name)[0]
+        res = self.get_result(category_name=category_name)[0]
+        if q_no and res != 0:
+            per = (res / q_no) * 100
+            return str(int(per))
+        else:
+            return "0"

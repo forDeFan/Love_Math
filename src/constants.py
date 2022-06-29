e557f53a9b5c2242b4869cb501f12f5546310b5d
tom <t-gutowski@o2.pl>
2022-06-29 18:27:28 +0200

    feat(colors): implement colors from constants.py in kv files

--- src/constants.py
+++ src/constants.py
@@ -4,3 +4,8 @@ APP_LANG = ["pl", "en"]
 # Categories en and pl have to correspond eachother at specific index.
 CATEGORIES = ["multiply", "fraction", "percent", "roman"]
 CATEGORIES_PL = ["mnożenie", "ułamki", "procenty", "liczby rzymskie"]
+# Colors.
+BLACK = (0, 0, 0, 1)
+GRAY = (128 / 255, 128 / 255, 128 / 255, 1)
+RED = (255 / 255, 0, 0, 1)
+PINK = (250 / 255, 232 / 255, 238 / 255, 1)

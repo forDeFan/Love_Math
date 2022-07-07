9bb68f5719e00be6f7e60126fd4fa6fc6194fb1b
tom <t-gutowski@o2.pl>
2022-07-07 17:54:53 +0200

    docs(docstrings)

--- src/android_helpers.py
+++ src/android_helpers.py
@@ -7,14 +7,41 @@ from plyer import sms
 
 
 class Android_Helpers:
+    """To be used on Android."""
+
     def get_ph_book(self) -> Dict[str, List[str]]:
+        """
+        Get phone book from device.
+
+        Returns:
+            Dict[str, List[str]]: Dict[contact name, List[of phone no's]].
+        """
         return get_contact_details("phone_book")
 
     def show_toast(self, text: str) -> None:
+        """
+        Show toast message onto device.
+
+        Args:
+            text (str): message to be shown.
+        """
         toast(text)
 
     def send_sms(self, tel: str, msg: str) -> None:
+        """
+        Send sms from device.
+
+        Args:
+            tel (str): receiver phone number
+            msg (str): message to be sent
+        """
         sms.send(recipient=tel, message=msg)
 
     def check_lang() -> str:
+        """
+        Detect device default language.
+
+        Returns:
+            str: returns os lang in LA_la format
+        """
         return device_lang()

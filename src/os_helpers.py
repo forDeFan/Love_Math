8dcfb7e898a8295c737a9621a742134d0d2339db
tom <t-gutowski@o2.pl>
2022-07-07 17:56:15 +0200

    docs(docstrings)

--- src/os_helpers.py
+++ src/os_helpers.py
@@ -2,7 +2,17 @@ import locale
 
 
 class Os_Helpers:
+    """
+    Class to interfere with os.
+    """
+
     def check_lang(self) -> str:
+        """
+        Os default lang check.
+
+        Returns:
+            str: in format: "en"
+        """
         locale.setlocale(locale.LC_ALL, "")
         lang = locale.getlocale(locale.LC_MESSAGES)[0][:-3]
         return lang

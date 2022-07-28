46ca174f43a237e3c19880b1ddf091f0baf94fbc
tom <t-gutowski@o2.pl>
2022-07-28 20:26:19 +0200

    test: resigned from mark.skip to mockig of locale

--- tests/test_os_helpers.py
+++ tests/test_os_helpers.py
@@ -1,21 +1,20 @@
-import pytest
+from pytest_mock import mocker
 from src.os_helpers import Os_Helpers
-import locale
 
 
 class Test_Os_Helpers:
     def test_if_short_lang_name_returned(self):
         lang = Os_Helpers.check_lang(Os_Helpers)
+
         assert len(lang) == 2
 
     def test_if_pl_lang_detected(self):
         pl_lang = Os_Helpers.check_lang(Os_Helpers)
+        
         assert pl_lang == "pl"
 
-    @pytest.mark.skipif(
-        locale.getlocale(locale.LC_MESSAGES) != "En_en",
-        reason="non english OS",
-    )
-    def test_if_en_lang_detected(self):
-        en_lang = Os_Helpers.check_lang(Os_Helpers)
-        assert en_lang == "en"
+    def test_if_en_lang_detected(self, mocker):
+        mocker.patch("locale.setlocale")
+        mocker.patch("locale.getlocale", return_value=[
+                     "en_En", "0", "0", "0"])
+        assert "en" in Os_Helpers.check_lang(Os_Helpers)

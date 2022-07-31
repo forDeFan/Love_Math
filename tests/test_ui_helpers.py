cda9e378ccac46b1cfd09f5fb8809a627470872b
tom <t-gutowski@o2.pl>
2022-07-31 11:05:08 +0200

    test: add basic cases for further code testing

---
+++ tests/test_ui_helpers.py
@@ -0,0 +1,17 @@
+import pytest
+from pytest_mock import mocker
+
+from src.ui_helpers import Ui_Helpers
+
+
+class Test_Ui_Helpers:
+
+    @pytest.fixture
+    def ui_hlp(self):
+        return Ui_Helpers()
+
+    def testing_widget_hiding(self, ui_hlp):
+        pass
+
+    def testing_custom_recycler(self, ui_hlp):
+        pass

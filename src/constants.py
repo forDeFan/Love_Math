fe9d18922edb69bd7a6944db5ca59026afa868a0
tom <t-gutowski@o2.pl>
2022-08-09 17:55:02 +0200

    feat: add new colors and ranges for screens

--- src/constants.py
+++ src/constants.py
@@ -1,7 +1,9 @@
 """
 Static values for centralized app flow.
+
 APP, DB and LANG set up here.
 COLORS set up here.
+SCREENS initial numbers/ ranges set up here.
 """
 
 APP_NAME = "I LoVE MatH"
@@ -15,3 +17,7 @@ BLACK = (0, 0, 0, 1)
 GRAY = (128 / 255, 128 / 255, 128 / 255, 1)
 RED = (255 / 255, 0, 0, 1)
 PINK = (250 / 255, 232 / 255, 238 / 255, 1)
+GREEN = (181 / 255, 255 / 255, 235 / 255, 1)
+# Screens setup at app start - can set up ranges before app start.
+ROMAN_NUMBERS_SCREEN_RANGE = (1, 1500)
+MULTIPLICATION_SCREEN_RANGE = [(2, 30), (2, 9)]

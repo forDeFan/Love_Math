bc3dde9652fd5b4b5e14f7f0ecf7fc0cf35626d1
tom <t-gutowski@o2.pl>
2022-08-10 21:11:25 +0200

    test(fix): change test to match multiplication changes

--- tests/test_multiplication.py
+++ tests/test_multiplication.py
@@ -11,18 +11,19 @@ class Test_Multiplication:
         return Multiply()
 
     def test_if_random_numbers_generated(self, multiply):
-        nums1 = multiply.generate_numbers()
-        multiply.generate_numbers()
-        nums2 = multiply.generate_numbers()
+        test_rang = ((1, 200), (3, 400))
+        nums1 = multiply.generate_num(test_rang)
+        nums2 = multiply.generate_num(test_rang)
 
         assert nums1 != nums2
 
     def test_if_generated_random_numbers_at_specified_range(
         self, multiply
     ):
-        r1 = range(2, 31)
-        r2 = range(2, 10)
-        nums = multiply.generate_numbers()
+        r1 = range(2, 32)
+        r2 = range(2, 11)
+        test_rang = ((2, 31), (2, 10))
+        nums = multiply.generate_num(test_rang)
 
         assert nums[0] in r1
         assert nums[1] in r2
@@ -33,11 +34,12 @@ class Test_Multiplication:
         num1 and num2 taken from Label.text attribute.
         """
         mocked_generate_numbers = mocker.patch.object(
-            multiply, 'generate_numbers')
+            multiply, 'generate_num')
         mocked_generate_numbers.return_value = [1, 1]
         lab = Label(text="")
         multiply.ids = {"num1": lab, "num2": lab}
-        multiply.set_nums(nums=[lab.text, lab.text])
+        multiply.set_num(ids_to_set=[multiply.ids["num1"], multiply.ids["num1"]],
+                         generated_nums=[lab.text, lab.text])
 
         assert multiply.ids.num1.text == "1"
 
@@ -47,6 +49,7 @@ class Test_Multiplication:
         """
         lab = Label(text="2")
         multiply.ids = {"num1": lab, "num2": lab}
-        multiply.set_nums(nums=[lab.text, lab.text])
+        multiply.set_num(ids_to_set=[
+            multiply.ids["num1"], multiply.ids["num2"]], generated_nums=[lab.text, lab.text])
 
         assert multiply.ids.num1.text == "2"

e1e0e85e1b0954729e004d8a57a8ea9d82f6c12c
tom <t-gutowski@o2.pl>
2022-09-27 17:51:09 +0200

    docs: add doccumentation feat: add screen inherotance thru Answer_Checker

--- src/roman_nums.py
+++ src/roman_nums.py
@@ -3,7 +3,6 @@ from typing import Tuple
 
 import roman
 from kivy.properties import NumericProperty
-from kivy.uix.screenmanager import Screen
 from kivy.utils import platform
 
 import src.helpers.constants as const
@@ -11,15 +10,40 @@ from src.abstract.calculation_abstract import Calculation_Abstract
 from src.answer_checker import Answer_Checker
 
 
-class Roman_Nums(Calculation_Abstract, Screen, Answer_Checker):
+class Roman_Nums(Calculation_Abstract, Answer_Checker):
+    """ 
+    Class for roman numbers.
+    Used in roman_nums_screen.kv
+    """
 
     # Taken from kv file.
     asked_question_no = NumericProperty(1)
 
     def generate_num(self, nums_range: Tuple[int, int]) -> str:
+        """
+        Generate random number in roman literal.
+
+        Args:
+            nums_range (Tuple[int, int]): range for randint to generate two numbers.
+
+        Returns:
+            str: roman literal of random generated number
+        """
         return roman.toRoman(randint(nums_range[0], nums_range[1]))
 
     def set_num(self, ids_to_set: str, generated_nums: str = None) -> None:
+        """
+        Set number in UI at roman_numbers_screen.kv.
+
+        This function used only when correct answer given or at first app run to populate it if UI field empty.
+        If leaving screen or wrong answer - roman number stay unchanged until task completed succesfullfy by user.
+
+        Args:
+            ids_to_set (str): id's taken from kv file to be changed.
+            generated_nums (str, optional): 
+                Randomly generated roman number literal. 
+                Defaults to None.
+        """
         n = ids_to_set
         if n.text == "":
             n.text = self.generate_num(
@@ -28,7 +52,26 @@ class Roman_Nums(Calculation_Abstract, Screen, Answer_Checker):
             n.text = generated_nums
 
     def show_result(self, ids_to_set, nums_range: Tuple[int, int]) -> bool:
-        # Unfocus textfield on android in order to receive key input.
+        """
+        Notify user in UI roman_number_screen.kv about right/ wrong result.
+        If correct answer arrange new roman task in UI.
+
+        Changes params of UI:
+            result_label,
+            result_text,
+            result_text.text,
+            check_button.
+
+        Args:
+            ids_to_set (_type_): id from kv file to be changed.
+            nums_range (Tuple[int, int]): Range for number generation by randint.
+
+        Returns:
+            bool (to notify Results.update_result() in kv file):
+                True if good answer.
+                False when wrong answer.
+                No bool returned when number/ char used by user in answer field - instead of number.
+        """
         if platform == "android":
             from src.helpers.android_helpers import Android_Helpers as an_hlp
 
